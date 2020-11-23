4197e050f713818953aca467db6e1abe50dcaf1f
LisandroCelis <lisandroceliscarvajal@gmail.com>
2020-11-22 19:46:14 -0500

    Caesar Cipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,27 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tudelft.mirror.Mirror;
 
+public class CaesarShiftCipherTest {
+    @Test
+    public void CaesarTest() {
+        CaesarShiftCipher caesar=new CaesarShiftCipher();
+        String result = caesar.CaesarShiftCipher("def",3);
+        Assertions.assertEquals("ghi", result);
+    }
+
+    @Test
+    public void CaesarTestAbc() {
+        CaesarShiftCipher caesar=new CaesarShiftCipher();
+        String result = caesar.CaesarShiftCipher("abc",3);
+        Assertions.assertEquals("def", result);
+    }
+    @Test
+    public void CaesarTestxyz() {
+        CaesarShiftCipher caesar=new CaesarShiftCipher();
+        String result = caesar.CaesarShiftCipher("xyz",3);
+        Assertions.assertEquals("abc", result);
+    }
 }
